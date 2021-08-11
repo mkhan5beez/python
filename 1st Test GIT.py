b286b7662c6c3c0cbeee4421ffb18e9d276c5765
mkhan5beez <68417155+mkhan5beez@users.noreply.github.com>
2021-08-11 10:10:04 +1000

    test

--- 1st Test GIT.py
+++ 1st Test GIT.py
@@ -1,1 +1,15 @@
+#This is Python Command
+x=10
+y=8
+z=x+y
+print(z)
 print ("Hello")
+
+x=abs(-4)
+type(x)
+import math
+
+print(math.sqrt(z))
+name="Masud"
+print(f"Hello {name}")
+print(name.lower())
